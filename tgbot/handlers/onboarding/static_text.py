af6c1424c8ff9c276efde77d286f44f27d31609d
Romzes <ggggrrrroomm@yandex.ru>
2024-04-23 21:51:43 +0300

    Initial

---
+++ tgbot/handlers/onboarding/static_text.py
@@ -0,0 +1,7 @@
+start_created = "Привет, {first_name}!\nЭто SciSource\nА это его описание\nПришли интересующий тебя запрос"
+start_not_created = "С возвращением, {first_name}!\nЭто SciSource\nА это его описание\nПришли интересующий тебя запрос"
+unlock_secret_room = "Congratulations! You've opened a secret room👁‍🗨. There is some information for you:\n" \
+           "<b>Users</b>: {user_count}\n" \
+           "<b>24h active</b>: {active_24}"
+github_button_text = "GitHub"
+secret_level_button_text = "Secret level🗝"
